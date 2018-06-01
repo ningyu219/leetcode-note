7a03331a60ff6ff7183540c1cee0690aad12345a
nina219 <ningyu219@gmail.com>
2018-06-01 11:33:51 +0800

    java project create

---
+++ leetcodeExec/src/leetcodeExec/Solution.java
@@ -0,0 +1,16 @@
+package leetcodeExec;
+
+public class Solution {
+	// 53. 最大子数组（动态规划）
+	public int maxSubArray(int[] nums) {
+        int maxSum=Integer.MIN_VALUE,tempSum=0;
+        for(int i=0; i<nums.length; i++){
+            tempSum = Math.max(nums[i], tempSum+nums[i]);
+            if(maxSum<tempSum)
+                maxSum = tempSum;
+            else if(tempSum<0)
+                tempSum = 0;
+        }
+        return maxSum;
+    }
+}
